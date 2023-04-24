4b0dea3f9d560b06ef7dff9666c7a030f3183711
Dr Elijah Borodin <104091990+elbor7@users.noreply.github.com>
2023-04-24 22:36:12 +0100

    Delete src directory

--- src/lib/PCC_Processing/functions/Processing_Imposed_functions.h
+++
@@ -1,5 +0,0 @@
-///================================ A part of the PCC Processing module =============================================================///
-///=================================================================================================================================///
-/** The library contains functions for generation of new identifiers for m-Cells, m={0,1,2} based on the already assigned types    **/
-/**  of the k-Cells with k > m, in the PCC Processing module. It created an "imposed" structure of m-Cells in a PCC.              **/
-///==============================================================================================================================///
