ed6cc7fb7f72dd0e7d5351de5261a7fd894aa2e0
elbor21 <44059223+elbor21@users.noreply.github.com>
2024-02-12 10:41:56 +0000

    Old Subcomplex with new working structure of the code (!)

--- src/lib/PCC_Multiphysics/functions/Multiphysics_functions.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef PCC_PROCESSING_DESIGN_MULTIPHYSICS_FUNCTIONS_H
-#define PCC_PROCESSING_DESIGN_MULTIPHYSICS_FUNCTIONS_H
-
-/// Set stress field for a Micro-crack (class Macrocrack)
-void crack_modes_stress_field(std::vector <double> &face_energies, int crack_mode, double new_crack_length, double external_vonMizes_stress, double Puasson_coeff = 0.3, double Young_modulus = 215.0*pow(10.0,9));
-
-#endif //PCC_PROCESSING_DESIGN_MULTIPHYSICS_FUNCTIONS_H
